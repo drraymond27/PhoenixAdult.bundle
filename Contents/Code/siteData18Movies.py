7c1dc737f90c79f7ea669cad3424884b7f037466
drraymond27 <67009823+drraymond27@users.noreply.github.com>
2021-12-25 07:28:47 -0500

    Update siteData18Movies.py

--- Contents/Code/siteData18Movies.py
+++ Contents/Code/siteData18Movies.py
@@ -186,8 +186,8 @@ def update(metadata, lang, siteNum, movieGenres, movieActors):
     detailsPageElements = HTML.ElementFromString(req.text)
 
     if len(metadata_id) > 3:
-        Log('Switching to Data18Content')
-        siteData18Content.update(metadata, lang, siteNum, movieGenres, movieActors)
+        Log('Switching to Data18Scenes')
+        siteData18Scenes.update(metadata, lang, siteNum, movieGenres, movieActors)
         return metadata
     
     # Title
